4fdef2f56640c7038e2d952f417b4fe8ee7a5195
Prateek Chanda <prateekkol21@gmail.com>
2021-08-09 22:23:51 +0530

    Update download_data.sh

--- bart/download_data.sh
+++ bart/download_data.sh
@@ -3,21 +3,21 @@
 
 BASE_URL=https://storage.googleapis.com/danielk-files/data
 
-if ($1 == "pre-training")
+if [ $1 = "pretrained" ]
 
 then
-    print "Downloading pre-training datasets"
+    echo "Downloading pre-training datasets"
     mkdir -p pretrain-data
     # datasets for pretraining
     for dataset in narrativeqa ai2_science_middle ai2_science_elementary arc_hard arc_easy mctest_corrected_the_separator squad1_1 squad2 boolq race_string openbookqa ; do
-        mkdir -p data/${dataset}
+        mkdir -p pretrain-data/${dataset}
         for data_type in train dev test ; do
             wget ${BASE_URL}/${dataset}/${data_type}.tsv -O pretrain-data/${dataset}/${data_type}.tsv
         done
     done
 
 else
-    print "Downloading other datasets"
+    echo "Downloading other datasets"
     mkdir -p data
     # other datasets
     for dataset in qasc qasc_with_ir commonseseqa openbookqa_with_ir arc_hard_with_ir arc_easy_with_ir winogrande_xl physical_iqa social_iqa ropes natural_questions_with_dpr_para ; do
@@ -27,8 +27,18 @@ else
         done
     done
 
+fi
 
 exit
+
+if [ $1 = "pretrained" ];
+
+then
+    wget https://nlp.cs.washington.edu/ambigqa/data/nqopen.zip -O pretrain-data/nqopen.zip
+    unzip -d pretrain-data pretrain-data/nqopen.zip
+    rm pretrain-data/nqopen.zip
+
+else
 
 wget https://nlp.cs.washington.edu/ambigqa/data/nqopen.zip -O data/nqopen.zip
 unzip -d data data/nqopen.zip
